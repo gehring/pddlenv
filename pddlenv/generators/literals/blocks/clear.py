73ef7fb509228211c4af53db7551ecb0c66096ac
gehring <clement.gehring@gmail.com>
2020-09-23 14:58:08 -0400

    Add more info in error message

--- pddlenv/generators/literals/blocks/clear.py
+++ pddlenv/generators/literals/blocks/clear.py
@@ -20,12 +20,14 @@ class ClearSampler(base.LiteralsSampler):
         if BLOCK_TYPE_NAME not in types:
             raise ValueError(
                 "Only blocks world problems are supported which must contain the following "
-                f"types: {BLOCK_TYPE_NAME} "
+                f"types: {BLOCK_TYPE_NAME}\n"
+                f"Found types: {set(types)}"
             )
         if not (PREDICATE_REQ - preds.keys()):
             raise ValueError(
                 "Only blocks world problems are supported which must contain the following "
-                f"predicates: {PREDICATE_REQ} "
+                f"predicates: {PREDICATE_REQ}\n"
+                f"Found predicates: {set(preds.keys())}"
             )
 
         blocks = [x for x in problem.objects if type(x).__name__ == BLOCK_TYPE_NAME]
